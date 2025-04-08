cdc0a78fd6f8d0e7f330ee286f8cefa452fb7356
papamanu <papamdoc1@gmail.com>
2025-04-08 12:36:19 -0400

    Points Store Skeleton Complete

--- my-app/src/pages/Store.jsx
+++ my-app/src/pages/Store.jsx
@@ -4,7 +4,15 @@ import '../App.css'
 const Store = () => {
     return (
         <div>
-            <p> this is the store page </p>
+            <p>This is the store page</p>
+
+            <ul>
+                <li>Pet collar: 500 points</li>
+
+                <li>Custom pet collar: 750 points</li>
+
+                <li>Chew toy: 1000 points</li>
+            </ul>
         </div>
     );
 };
