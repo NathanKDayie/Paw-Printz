eacd3088fad246decd66d1fd3592e91473ca4c0f
Kellee Palmer <kelleespalmer@gmail.com>
2025-05-06 12:11:34 -0400

    authentication fixes

--- client/src/Authentication.jsx
+++ client/src/Authentication.jsx
@@ -2,16 +2,26 @@ import { getAuth, signInWithPopup, GoogleAuthProvider, setPersistence, browserSe
 
 const auth = getAuth();
 const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+  // Restrict sign-in to UMBC Google accounts
+  hd: "umbc.edu",
+});
 provider.addScope('https://www.googleapis.com/auth/userinfo.profile');
 provider.addScope('https://www.googleapis.com/auth/userinfo.email');
 
 export const handleGoogleSignIn = async () => {
     try {
-        await setPersistence(auth, browserSessionPersistence);
+      await setPersistence(auth, browserSessionPersistence);
       const result = await signInWithPopup(auth, provider);
   
       const user = result.user;
-      //console.log('User signed in:', user);
+      const email = user.email;
+      if (!email || !email.endsWith("@umbc.edu")) { //signs them out if they are not a UMBC account
+        await auth.signOut();
+        throw new Error("Access denied. You must use a UMBC account.");
+      }
+  
+      console.log("User signed in with UMBC account:", email);
   
     } catch (error) {
       console.error('Error during sign-in:', error);
